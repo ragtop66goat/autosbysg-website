8ec2aaec7f71a716317cfb382cd54f8c70f84217
ragtop66goat <ragtop66goat@gmail.com>
2023-08-04 17:03:51 -0500

    resolved warnings for deployment

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -28,6 +28,7 @@ export function About() {
                 </div>
             </div>
             <iframe
+                title="address"
                 className="i-frame"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2990.0797507344996!2d-81.73386048457199!3d41.4591855792577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8830efd4651d8885%3A0x4552ebb36f5c1b17!2s6409%20Denison%20Ave%2C%20Cleveland%2C%20OH%2044102!5e0!3m2!1sen!2sus!4v1617767616278!5m2!1sen!2sus"
                 allowFullScreen=""
