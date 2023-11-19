e5f67ccbab560c0eab7d3fc0523912fff18d0cf5
ragtop66goat <ragtop66goat@gmail.com>
2023-11-18 20:22:03 -0600

    added tests for CarouselItem and hero

--- src/components/carousel item/CarouselItem.js
+++ src/components/carousel item/CarouselItem.js
@@ -3,10 +3,10 @@ import {Card} from "react-bootstrap";
 export function CarouselItem({item}){
     return (
         <Card className="p-2 m-2 carousel-card">
-            <div className="card-image" style={{backgroundImage: `url(${item.src})`}}></div>
+            <div data-testid="item-image" className="card-image" style={{backgroundImage: `url(${item.src})`}}></div>
             <div className="card-body">
                 <h5 className="card-title">{item.title}</h5>
-                <p className="card-text">Miles: {item.miles}<br/> Price: ${item.miles}</p>
+                <p className="card-text">Miles: {item.miles}<br/> Price: ${item.price}</p>
             </div>
         </Card>
     )
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,8 +3,8 @@ import "./hero.css";
 export function Hero() {
     return (
         <div data-testid="hero" className="showcase">
-            <h1>S-G <span>Auto Sales</span></h1>
-            <p>Stop in and test drive your new car today</p>
+            <h1 data-testid="hero-banner">S-G <span>Auto Sales</span></h1>
+            <p data-testid="hero-message">Stop in and test drive your new car today</p>
         </div>
     )
 }
